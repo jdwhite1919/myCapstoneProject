fc2139e39833e48617c582ca81994ae858f1f8c1
JD White <jdwhite1919@gmail.com>
2020-10-27 14:40:41 -0500

    update capstone repo

--- components/Nav.js
+++ components/Nav.js
@@ -2,13 +2,13 @@ export default links => `
 <header>
 <div class="nav-bar">
 
-    <p class="logo">J/D Fit!</p>
+    <p class="logo">Pack Fitness</p>
 
     <nav>
       <ul>
         <li><a href="#">Why Start</a></li>
         <li><a href="#">Getting Motivated</a></li>
-        <li><a href="components/views/Calculator.js">Tools to Succeed</a></li>
+        <li><a href="/Calculator.js" data-navigo>Tools to Succeed</a></li>
         <li><a href="#">Staying Healthy</a></li>
         <li><a href="#">Let Us Help!</a></li>
       </ul>
--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,12 @@
 export default () => `
 <h1>Fitness in the Time of Covid</h1>
 
+<div class="homePageText">
+<div class="introduction-box">
+<img src="images/battleropes.jpg" alt="battleropes" title="battleropes" />
 <p>Covid-19 has impacted all aspects of life, and fitness is no exception. With many working from home, gyms closed or with reduced hours or restrictions, and the general fear of spreading the disease to begin with, it has never been harder to start a healthy lifestyle. Unfortunately, health is more important now than ever. So what is one to do?</p>
+</div>
 
-<p>In the middle of difficulty lies opportunity. </p>
+<p>Albert Einstein once said, "In the middle of difficulty lies opportunity." Fitness is always a hard road to follow, and 2020 has only made it harder. But this could also be an opportunity. With the global pandemic, now could be the time to realize health is more important than ever. It can keep us mentally, emotionally, and physically ready for the uncertainty in this world and provide a framework of example for those you surround yourself with. Now can be a perfect opportunity for a life-changing decision.</p>
+</div>
 `;
