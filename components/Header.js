fa733354cdbffd6352954b58040127eb2d574968
JD White <jdwhite1919@gmail.com>
2020-10-27 16:08:40 -0500

    update capstone repo

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,18 @@
 export default st => `
+<header>
+<div class="nav-bar">
 
+    <p class="logo">Pack Fitness</p>
+
+    <nav>
+      <ul>
+        <li><a href="#">Why Start</a></li>
+        <li><a href="#">Getting Motivated</a></li>
+        <li><a href="/Calculator.js" data-navigo>Tools to Succeed</a></li>
+        <li><a href="#">Staying Healthy</a></li>
+        <li><a href="#">Let Us Help!</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
 `;
