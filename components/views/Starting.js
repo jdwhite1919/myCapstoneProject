96ed7dd422e4fab5e53a468a1a0c949b904a9cf8
JD White <jdwhite1919@gmail.com>
2020-10-30 11:30:29 -0500

    update capstone repo

--- components/views/Starting.js
+++ components/views/Starting.js
@@ -1,3 +1,95 @@
 export default () => `
+<h1>Weights or Cardio or Both, oh My!</h1>
 
+<h2 id="hitting-weights">Hitting Weights</h2>
+
+<img class="weights-image" src="weights.jpeg"/>
+
+<p>
+"Lightweight, baby!!" "Get it!!" Screams Echos Through the Gym*
+<br>
+Things absolutely necessary to say when lifting since lifting weights is only for lunks, right??
+<br>
+WRONG!!
+<br>
+Almost everyone of all ages should be doing weight-bearing activity in one way or another.
+Lifting weights is for women, men, adolescents, geriatrics, pediatrics, you name it. Benefits of lifting weights across all demographics include (but are not limited to!):
+<br>
+<ul>
+  <li>Increase in Bone Density</li>
+  <li>Peak Flexibility and Mobility (Full Range of Motion)</li>
+  <li>Fat Loss</li>
+  <li>Neuromuscular Efficiency</li>
+  <li>Testosterone Production and Release (Important for Men AND Women)</li>
+</ul>
+<br>
+<p>
+Weight-bearing fitness is crucial to health, and beneficial in fighting common chronic conditions such as cardiovascular disease (the number one killer of Americans), osteoporosis, arthritis, diabetes, just to name a few. Ready to improve your quality of life? Hit the weights.
+<br>
+<a href="https://www.dummies.com/health/exercise/weights/weight-lifting-safety-tips/">Safety Guide to Weights</a>
+</p>
+</p>
+
+<h2 id="hitting-cardio">Hitting Cardio</h2>
+
+<img class="cardio-image" src="cardio.jpeg"/>
+
+<p>
+Aww, the dreaded cardiovascular exercise. Something almost universally hated yet something almost universally used. People think cardio is the life force of fitness, like oxygen is to the body. Many people it burns fat and gives us that athletic look so deeply coveted by many. Well, not exactly.
+<br>
+Don't get me wrong, cardio will still improve such aspects as:
+<br>
+<ul>
+  <li>Cardiovascular Endurance</li>
+  <li>Vo2 Max</li>
+  <li>Burn calories (but theres a catch)</li>
+  <li>Improve REM cycles and boost moods (we love endorphins!)</li>
+</ul>
+<br>
+<p>
+There are a few things to be aware of with cardio. Remember there are three different types of bpm categories that define how intense a cardio-workout session is; check with your doctor to see if you are safe for all three of them. Additionally, many HIIT workouts (high-intensity interval training) are high impact workouts that are hard on the joints; check to see if you are able to handle these high impact workouts and practice them first in a slower manner to practice form (like you would with weights). Cardio workouts are great, just know how to use it to your best benefit to reach your own goals.
+<br>
+Here is a link to help you calculate your max bpm for cardio workouts.
+<br>
+<a href="https://www.active.com/fitness/calculators/heartrate">BPM Calculator</a>
+</p>
+</p>
+
+<h2 id="both">So Which One Is For Me?</h2>
+<p>
+Alright, so you got yourself off the couch and you are at the gym. Which one should you do? The weights look intimidating and the cardio equipment looks just plain boring.
+<br>
+Of course, the most optimal answer is for you do both, focusing on one and using the other as a tool in your arsenal. Here is how you should break it down if you decide to do both.
+</p>
+<h4>Weights First</h4>
+<p>
+Weights come first at the gym if these are your goals.
+</p>
+  <ul>
+    <li>Fat Loss</li>
+    <li>Build Strength</li>
+    <li>Improve Coordination</li>
+    <li>Testosterone and Growth Hormone Release</li>
+  </ul>
+  <p>
+  While most think the infamous "athletic" looks comes from cardio, weights are actually the way to go. In order to target fat loss specifically, glycogen stores in the body need to be burned while weighlifting, so by the time cardio rolls around in the workout, you can optimize the "fat-burning zone" and use triglycerides as energy. Now remember, you cannot turn fat in to muscle, but building muscle also helps burn fat while sitting at home on the couch.
+  </p>
+<h4>Cardio First</h4>
+<p>
+Most people find themselves in the goals that correlate to hitting the weights first in a workout, but a few circumstances can dictate when to hit cardio first.
+</p>
+<ul>
+  <li>Build Cardiovascular Endurance</li>
+  <li>Overall Weight Loss</li>
+  <li>Vo2 Max Improvement</li>
+</ul>
+<p>
+Basically, if you are training for a race or another type of endurance event, than hitting cardio first is the way to go. Also, with overall weight loss, remember you might see results sooner, but since you run the risk of burning muscle, the results may not stick as long if you fall off the loop.
+</p>
+
+<br>
+
+<p>
+The choice is yours. Think about your goals and execute your own personal plan based on it; your goals are your goals, and they can be as similar or as different to others' as you want them to be. Fitness is as individual as it is social, and no one can tell you what is right for you!
+</p>
 `;
